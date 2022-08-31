14d5ab35573d34fcf52fe0ac909773d3f0ad8f8f
rizdiaprilian <rizdi.shusuke@gmail.com>
2022-08-31 14:25:20 +0000

    add sh file

---
+++ UK_house_price/run_prefect.sh
@@ -0,0 +1,20 @@
+### Before running Prefect Orion, set two configurations that matches with current IP address
+prefect config set PREFECT_ORION_UI_API_URL="http://18.133.194.13:4200/api"
+
+prefect config set PREFECT_API_URL="http://18.133.194.13:4200/api"
+
+### You can run Prefect Orion after getting done above
+prefect orion start --host 0.0.0.0
+
+### Do this after setting mlflow.set_tracking_uri(f"http://{TRACKING_SERVER_HOST}:5000")
+### Specifically for project UK_house_price
+mlflow server -h 0.0.0.0 -p 5000 --backend-store-uri sqlite:///mlflow_uk_house.db --default-artifact-root s3://mlopszoomcamp-bucket
+
+### Chance Public IP4v address each time EC2 runs each time
+http://ec2-18-133-194-13.eu-west-2.compute.amazonaws.com:5000
+
+export MLFLOW_TRACKING_URI="http://ec2-18-133-194-13.eu-west-2.compute.amazonaws.com:5000"
+
+### Alternatively start mlflow with this command 
+mlflow ui -h 0.0.0.0 -p 5000 --backend-store-uri sqlite:///mlflow_uk_house.db \
+        --default-artifact-root s3://mlopszoomcamp-bucket
