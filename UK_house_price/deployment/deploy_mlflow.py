e53cd18cb44b0bcffdff5e45d8035f8d73be7841
rizdiaprilian <rizdi.shusuke@gmail.com>
2022-09-05 10:02:03 +0000

    Add file deploy python

---
+++ UK_house_price/deployment/deploy_mlflow.py
@@ -0,0 +1,60 @@
+import os
+from datetime import datetime
+import pandas as pd
+import mlflow
+from mlflow.tracking import MlflowClient
+from flask import Flask, request, jsonify
+import boto3
+
+
+RUN_ID = os.getenv('RUN_ID', "a83db8840d254e9095dfe0ed2bc92158")
+S3_BUCKET = os.getenv('S3_BUCKET', "mlopszoomcamp-bucket")
+REGION = os.getenv('REGION', "Oxford")
+### with MLFLow tracking server ###
+# RUN_ID = "run_id"
+# MLFLOW_TRACKING_URI = "public IPv4 DNS"
+
+# mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
+# client = MlflowClient(tracking_uri=MLFLOW_TRACKING_URI)
+
+# logged_model = f'runs:/{RUN_ID}/models_prophet'
+
+### from S3 without tracking server ###
+logged_model = f's3://{S3_BUCKET}/UK_house_price/{RUN_ID}/artifacts/models_prophet'
+model = mlflow.pyfunc.load_model(logged_model)
+
+def prepare_features(df_test):
+    features = {}
+    features['ds'] = df_test['ds']
+    features['y'] = df_test['y']
+
+    feature_df = pd.DataFrame.from_dict(features)
+    return feature_df
+
+
+def predict(features):
+    preds = model.predict(features)
+    return preds.to_dict("records")
+
+
+app = Flask('price-forecasting')
+
+@app.route('/predict', methods=['POST'])
+def predict_endpoint():
+    ride = request.get_json()
+
+    features = prepare_features(ride)
+    pred = predict(features)
+    result = {
+        'y_hat': pred[0]['yhat'],
+        'y_hat_lower' : pred[0]['yhat_lower'],
+        'y_hat_upper' : pred[0]['yhat_upper'],
+        'date': datetime.strftime(pred[0]['ds'], "%Y-%m-%d"),
+        'model_version': RUN_ID,
+        'region': REGION
+    }
+    return jsonify(result)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=9696)
