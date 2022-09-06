bda3b2f36e5cd32c86460bd4fb12aeb465cfa89e
Rizdi Aprilian <42743243+rizdiaprilian@users.noreply.github.com>
2022-09-06 11:55:59 +0100

    Delete UK_house_price/test directory

--- UK_house_price/test/copy_command.sh
+++
@@ -1,2 +0,0 @@
-## Create new model which directly populate items from s3 bucket
-aws s3 cp --recursive s3://mlopszoomcamp-bucket/UK_house_price/a83db8840d254e9095dfe0ed2bc92158/artifacts/models_prophet/ model
