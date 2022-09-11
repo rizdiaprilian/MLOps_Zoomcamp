0e9ab017df69dd23fb352f43dc8982156b05da85
rizdiaprilian <rizdi.shusuke@gmail.com>
2022-09-11 14:59:53 +0000

    minor update

--- UK_house_price/run_prefect.sh
+++ UK_house_price/run_prefect.sh
@@ -1,7 +1,7 @@
 ### Before running Prefect Orion, set two configurations that matches with current IP address
-prefect config set PREFECT_ORION_UI_API_URL="http://18.170.70.140:4200/api"
+prefect config set PREFECT_ORION_UI_API_URL="http://3.11.9.244:4200/api"
 
-prefect config set PREFECT_API_URL="http://18.170.70.140:4200/api"
+prefect config set PREFECT_API_URL="http://3.11.9.244:4200/api"
 
 ### You can run Prefect Orion after getting done above
 prefect orion start --host 0.0.0.0
@@ -11,11 +11,11 @@ prefect orion start --host 0.0.0.0
 mlflow server -h 0.0.0.0 -p 5000 --backend-store-uri sqlite:///mlflow_uk_house.db --default-artifact-root s3://mlopszoomcamp-bucket
 
 ### Export tracking uri that takes the Public IP4v address
-export MLFLOW_TRACKING_URI="http://ec2-18-170-70-140.eu-west-2.compute.amazonaws.com:5000"
+export MLFLOW_TRACKING_URI="http://ec2-3-11-9-244.eu-west-2.compute.amazonaws.com:5000"
 
 ### Alternatively start mlflow with this command. Don't forget to exit from pipenv shell
 mlflow ui -h 0.0.0.0 -p 5000 --backend-store-uri sqlite:///mlflow_uk_house.db \
         --default-artifact-root s3://mlopszoomcamp-bucket
 
 ### Copy this code to load MLFlow
-http://ec2-18-170-70-140.eu-west-2.compute.amazonaws.com:5000
+http://ec2-3-11-9-244.eu-west-2.compute.amazonaws.com:5000
